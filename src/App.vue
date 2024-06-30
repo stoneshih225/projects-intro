222a23100ae93289ab68fac3c720d14b8f27f244
stoneshih225 <sam23518020@gmail.com>
2024-06-30 16:07:39 +0800

    feat: 🎸 更新 App 組件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
+import EventDemo from '@/components/EventDemo/EventDemo.vue';
 </script>
 
 <template>
     <div class="app">
-    <!--  -->
+        <div class="app-wrap">
+            <EventDemo />
+        </div>
     </div>
 </template>
 
 <style lang="scss">
 @import "./styles/styles";
+
+.app {
+    position: relative;
+    min-width: 480px;
+    min-height: 100vh;
+
+    .app-wrap {
+        position: relative;
+        padding: 2.5rem 2rem;
+        width: 100%;
+        height: 100%;
+
+        @media (min-width: 576px) {
+            margin: 0 auto;
+            width: 576px;
+        }
+
+        @media (min-width: 992px) {
+            width: 992px;
+        }
+
+        @media (min-width: 1280px) {
+            width: 1280px;
+        }
+    }
+}
 </style>
