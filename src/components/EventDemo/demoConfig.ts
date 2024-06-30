55b63721987176e0022790793be7f06bd5bce65d
stoneshih225 <sam23518020@gmail.com>
2024-06-30 16:56:33 +0800

    fix: 🐛 修正圖片路徑

--- src/components/EventDemo/demoConfig.ts
+++ src/components/EventDemo/demoConfig.ts
@@ -11,7 +11,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 1,
         link: 'https://bbin-news.com/event_custom/F23/#/',
-        img: '/event_demo_1.jpg',
+        img: './event_demo_1.jpg',
         alt: '開運火箭',
         title: '開運火箭',
         tag: '#Vue2'
@@ -19,7 +19,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 2,
         link: 'https://bbin-news.com/event_custom/F38/#/',
-        img: '/event_demo_2.jpg',
+        img: './event_demo_2.jpg',
         alt: '錘子機',
         title: '錘子機',
         tag: '#Vue2'
@@ -27,7 +27,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 3,
         link: 'https://bbin-news.com/event_custom/F42/#/',
-        img: '/event_demo_3.jpg',
+        img: './event_demo_3.jpg',
         alt: '五一元宇宙',
         title: '五一元宇宙',
         tag: '#Vue2'
@@ -35,7 +35,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 4,
         link: 'https://bbin-news.com/event_custom/F48/#/',
-        img: '/event_demo_4.jpg',
+        img: './event_demo_4.jpg',
         alt: '金球入門',
         title: '金球入門',
         tag: '#Vue2'
@@ -43,7 +43,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 5,
         link: 'https://bbin-news.com/event_custom/F50/#/',
-        img: '/event_demo_5.jpg',
+        img: './event_demo_5.jpg',
         alt: '財神紅包',
         title: '財神紅包',
         tag: '#Vue2'
@@ -51,7 +51,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 6,
         link: 'https://bbin-news.com/event_custom/F52/#/',
-        img: '/event_demo_6.jpg',
+        img: './event_demo_6.jpg',
         alt: '新春盲盒',
         title: '新春盲盒',
         tag: '#Vue2'
@@ -59,7 +59,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 7,
         link: 'https://bbin-news.com/event_custom/F54/#/',
-        img: '/event_demo_7.jpg',
+        img: './event_demo_7.jpg',
         alt: '端午情至',
         title: '端午情至',
         tag: '#Vue2'
@@ -67,7 +67,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 8,
         link: 'https://bbin-news.com/event_custom/F56/#/',
-        img: '/event_demo_8.jpg',
+        img: './event_demo_8.jpg',
         alt: '情人樂透',
         title: '情人樂透',
         tag: '#Vue2'
@@ -75,7 +75,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 9,
         link: 'https://bbin-news.com/event_custom/F61/#/',
-        img: '/event_demo_9.jpg',
+        img: './event_demo_9.jpg',
         alt: '中秋博餅',
         title: '中秋博餅',
         tag: '#Vue2'
@@ -83,7 +83,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 10,
         link: 'https://www.bbinpromo.com/bbsport/event/202112/index.html#/',
-        img: '/event_demo_10.jpg',
+        img: './event_demo_10.jpg',
         alt: '足球推廣頁',
         title: '足球推廣頁',
         tag: '#Vue2'
@@ -91,7 +91,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 11,
         link: 'https://www.bbinpromo.com/newbbsport/event/202112/index.html#/',
-        img: '/event_demo_11.jpg',
+        img: './event_demo_11.jpg',
         alt: '籃球推廣頁',
         title: '籃球推廣頁',
         tag: '#Vue2'
@@ -99,7 +99,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 12,
         link: 'https://bbin-news.com/event_custom/E03/#/',
-        img: '/event_demo_12.jpg',
+        img: './event_demo_12.jpg',
         alt: '聖誕小瑪莉',
         title: '聖誕小瑪莉',
         tag: '#Vue3'
@@ -107,7 +107,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 13,
         link: 'https://bbin-news.com/event_custom/E04/#/',
-        img: '/event_demo_13.jpg',
+        img: './event_demo_13.jpg',
         alt: '迎春小瑪莉',
         title: '迎春小瑪莉',
         tag: '#Vue3'
@@ -115,7 +115,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 14,
         link: 'https://bbin-news.com/event_custom/E08/#/',
-        img: '/event_demo_14.jpg',
+        img: './event_demo_14.jpg',
         alt: '禮物樹',
         title: '禮物樹',
         tag: '#Vue3'
@@ -123,7 +123,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 15,
         link: 'https://bbin-news.com/event_custom/E09/#/',
-        img: '/event_demo_15.jpg',
+        img: './event_demo_15.jpg',
         alt: '百萬糖果樹',
         title: '百萬糖果樹',
         tag: '#Vue3'
@@ -131,7 +131,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 16,
         link: 'https://bbin-news.com/event_custom/E10/#/',
-        img: '/event_demo_16.jpg',
+        img: './event_demo_16.jpg',
         alt: '搖錢樹',
         title: '搖錢樹',
         tag: '#Vue3'
@@ -139,7 +139,7 @@ const demoConfig: DemoConfig[] = [
     {
         id: 17,
         link: 'https://bbin-news.com/event_custom/E16/#/',
-        img: '/event_demo_17.jpg',
+        img: './event_demo_17.jpg',
         alt: '蜂賞撈金',
         title: '蜂賞撈金',
         tag: '#Vue3'
