cba0c6edfef0a262435d7d6d585eb4de6d021245
stoneshih225 <sam23518020@gmail.com>
2024-06-30 12:29:44 +0800

    feat: 🎸 設定 lint 相關內容

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        eslintPlugin({ eslintOptions: { cache: false } })
+    ],
     resolve: {
         alias: [
             { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
